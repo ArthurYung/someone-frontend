69a5d01bb3a1a5ec85ebbe57f8e8ba192d808542
bruceouyang <bruceouyang@tencent.com>
2023-06-19 15:49:31 +0800

    feat: 新增pwa配置

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import "whatwg-fetch";
 import ReactDOM from 'react-dom/client'
 import {App} from './App.tsx'
 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
 )
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import autoprefixer from "autoprefixer";
 import { createHtmlPlugin } from "vite-plugin-html";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,26 @@ export default defineConfig({
     createHtmlPlugin({
       minify: true,
     }),
+    VitePWA({
+      includeAssets: ['someone-pwa.png'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Someone',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'someone-pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'someone-pwa.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   css: {
     postcss: {
