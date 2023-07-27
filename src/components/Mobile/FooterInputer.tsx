9376e825aa0e8d54e96910313baaca91d8728cf5
bruceouyang <bruceouyang@tencent.com>
2023-07-27 19:45:05 +0800

    feat: 优化一些功能

--- src/components/Mobile/FooterInputer.tsx
+++ src/components/Mobile/FooterInputer.tsx
@@ -1,82 +1,114 @@
-import { createRef, forwardRef, useEffect, useState } from "react";
-import { createRoot } from "react-dom/client";
-import { useWindowSize } from 'react-use';
-import { useVisibleAnimate } from "./useAnimate";
+import { createRef, forwardRef, useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { useVisibleAnimate } from './useAnimate';
+import { registeDestory } from './footerHelper';
 import './Footer.scss';
-import { registeDestory } from "./footerHelper";
 
-const cls = "mobile-footer";
-
+const cls = 'mobile-footer';
 
 interface FooterRadioMethods {
   remove: () => Promise<void>;
 }
 
 function stop(e: TouchEvent) {
-    e.preventDefault();
+  e.preventDefault();
+}
+
+function scrollEditorViewToEnd() {
+  const editorView = document.querySelector('.someone-editor') as HTMLElement;
+  if (!editorView) return;
+  const top = editorView.scrollHeight + editorView.offsetHeight;
+  editorView.scrollTo({ left: 0, top });
 }
 
 const useInputerFoucs = () => {
-    const [isFocus, setIsFocus] = useState(false);
+  const [isFocus, setIsFocus] = useState(false);
 
-    function focus() {
-        setIsFocus(true);
-        document.body.addEventListener('touchmove', stop, {
-            passive: false,
-        });
-    }
+  function focus() {
+    setIsFocus(true);
+    scrollEditorViewToEnd();
+    document.body.addEventListener('touchmove', stop, {
+      passive: false,
+    });
+  }
 
-    function blur() {
-        setIsFocus(false)
-        document.body.removeEventListener('touchmove', stop);
-    }
+  function blur() {
+    setIsFocus(false);
+    document.body.removeEventListener('touchmove', stop);
+  }
 
-    useEffect(() => blur,[]);
+  useEffect(() => blur, []);
 
-    return {
-        focus, blur, isFocus
-    }
-}
+  return {
+    focus,
+    blur,
+    isFocus,
+  };
+};
 
 export const FooterInputer = forwardRef<
-FooterRadioMethods,
+  FooterRadioMethods,
   {
     onSubmit: (val: string) => void;
-    placeholder: string,
+    placeholder: string;
   }
 >(({ onSubmit, placeholder }, ref) => {
   const [val, updateVal] = useState('');
   const { visibleState } = useVisibleAnimate(ref);
   const { focus, blur, isFocus } = useInputerFoucs();
+  function submitInputer() {
+    onSubmit(val);
+    updateVal('');
+  }
+
   return (
     <div className={cls}>
-        <div className={`${cls}--inner ${visibleState}`}>
-            <input placeholder={placeholder} value={val} onChange={e => updateVal(e.target.value)} type="text" className={`${cls}--inputer ${isFocus ? 'focus' : ''}`} onFocus={focus} onBlur={blur} />
-            <div className={`${cls}--item submit`} onClick={() => {
-              onSubmit(val);
-              updateVal('');
-            }}>↑</div>
+      <div className={`${cls}--inner ${visibleState}`}>
+        <form action="javascript:void(0)" onSubmit={(e) => e.preventDefault()} className={`${cls}--inputer-target  ${isFocus ? 'focus' : ''}`}>
+          <input
+            placeholder={placeholder}
+            value={val}
+            onChange={(e) => updateVal(e.target.value)}
+            type="text"
+            className={`${cls}--inputer`}
+            onFocus={focus}
+            onBlur={blur}
+            onKeyDown={(e) => e.key === 'Enter' && submitInputer()}
+          />
+        </form>
+        <div
+          className={`${cls}--item submit`}
+          onClick={submitInputer}
+        >
+          ↑
         </div>
+      </div>
     </div>
   );
 });
 
 export const CreateFooterInputer = (config: {
   onSubmit: (val: string) => void;
-  placeholder?: string
+  placeholder?: string;
 }) => {
-  const el = document.createElement('div')
+  const el = document.createElement('div');
   const root = createRoot(el);
   const ref = createRef<FooterRadioMethods>();
 
   document.body.appendChild(el);
-  root.render(<FooterInputer ref={ref} placeholder={config.placeholder || '请输入'} onSubmit={config.onSubmit}/>);
+  root.render(
+    <FooterInputer
+      ref={ref}
+      placeholder={config.placeholder || '请输入'}
+      onSubmit={config.onSubmit}
+    />
+  );
 
   return {
     destory: registeDestory(() => {
       ref.current?.remove().then(() => {
         root.unmount();
-      })
-    })
-  }
-}
+      });
+    }),
+  };
+};
