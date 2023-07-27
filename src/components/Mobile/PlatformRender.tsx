58c9fb4e20e7a3814d2ccad4b285a0f7980e9f15
bruceouyang <bruceouyang@tencent.com>
2023-07-27 16:14:44 +0800

    fix: ts lint

--- src/components/Mobile/PlatformRender.tsx
+++ src/components/Mobile/PlatformRender.tsx
@@ -4,6 +4,6 @@ import { isMobile } from "../ErrorPage/check";
 const isMobilePlatform = isMobile();
 
 export const PlatformRender =
-  (PC: ComponentType<any>, Mobile: ComponentType<any>): FC<{ children: any }> =>
+  (PC: ComponentType<any>, Mobile: ComponentType<any>): FC<{ children?: any }> =>
   ({ children }) =>
     isMobilePlatform ? <Mobile>{children}</Mobile> : <PC>{children}</PC>;
